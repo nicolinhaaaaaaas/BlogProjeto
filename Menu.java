2a095d8a1d5fed4bb8f288bed1923d333ca7a71e
Nicolas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-08-12 13:03:46 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Menu {
+	
+	static ArrayList<Usuario> usuarios = new ArrayList<>();
 
 	static void cadastrarUsuario() {
 		Scanner scanner = new Scanner(System.in);
@@ -19,18 +21,21 @@ public class Menu {
 			String email = scanner.next();
 			System.out.print("Digite sua senha: ");
 			String senha = scanner.next();
-			System.out.print("Digite a data de hoje no formato (dd-MM-yyyy): ");
-			String data = scanner.next();
+			//System.out.print("Digite a data de hoje no formato (dd-MM-yyyy): ");
+			//String data = scanner.next();
 			System.out.print("Escolha o seu cargo (1)-Convidado ; (2)-Admin ");
 			String cargo = scanner.next();
-			if(cargo.equals("1")) {
-				cargo = ("Convidado");
+			
+			CargoUsuario escolha = null;
+			
+			if(cargo.equals("2")) {
+				escolha = CargoUsuario.ADMIN;
 			}
-			else if(cargo.equals("2")) {
-				cargo = ("Admin");
+			else if(cargo.equals("1")) {
+				escolha = CargoUsuario.CONVIDADO;
 			}
 			
-			SimpleDateFormat inputFormat = new SimpleDateFormat("dd-MM-yyyy");
+			/*SimpleDateFormat inputFormat = new SimpleDateFormat("dd-MM-yyyy");
 	        Date date = null;
 
 	        try {
@@ -38,13 +43,36 @@ public class Menu {
 	            System.out.println("Data convertida: " + date);
 	        } catch (ParseException e) {
 	            System.out.println("Erro na conversão da data: " + e.getMessage());
-	        }
+	        } */
 	        
-	        Usuario usuario = new Usuario()
+	        int id = usuarios.size() +1;
+	        
+	        Usuario usuario = new Usuario(id, nome, email, senha,  escolha);
+	        
+	        usuarios.add(usuario);
+	        
+	        System.out.println(usuario);
+	        
+	        System.out.println(usuarios);
+	        
+	        
 		}
+		
+		else {
+			System.out.println("ok lol");
+		}
+		
+		scanner.close();
 	}
 	
 	static void loginUsuario() {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite seu login: ");
+		String login = scanner.next();
+		System.out.print("Digite sua senha: ");
+		String senha = scanner.next();
 		
 	}
 	
